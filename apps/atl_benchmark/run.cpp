0795843c0fb66faeadd2ef2bbcf8ab1e7fbce3b7
Amanda Liu <al3623@columbia.edu>
2021-06-29 22:24:17 -0400

    Add extern in runner

--- apps/atl_benchmark/run.cpp
+++ apps/atl_benchmark/run.cpp
@@ -12,9 +12,11 @@
 #include "blur_two_stage_float.h"
 #include "blur_tiled_float.h"
 
+extern "C" {
 #include "blurpart.h"
 #include "blurtwopart.h"
 #include "blurtiles.h"
+}
 
 // We want to continue to use our Halide::Buffer with AOT-compiled
 // code, so we explicitly include it. It's a header-only class, and
